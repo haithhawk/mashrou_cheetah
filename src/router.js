93c0f96e17b1e493dbf93902ff5a276fc8e08c9d
nicholaspeal <nicholaspeal@gmail.com>
2017-11-29 14:34:25 +0200

    Add Readme Reference #13

--- src/router.js
+++ src/router.js
@@ -4,23 +4,18 @@ const router=(req,res) =>{
 
 const url = req.url;
 console.log(url);
-if (url==='/'){
-
-staticHandler(__dirname+'/../index.html',res, 'text/html');
-}
-else if (url.split('.')[1]=== 'js') {
+    if (url==='/'){
+  staticHandler(__dirname+'/../index.html',res, 'text/html');
+  }
+    else if (url.split('.')[1]=== 'js') {
   staticHandler(__dirname+'/..'+url, res, 'application/javascript');
-
-}
-else if (url.split('.')[1]=== 'css') {
+  }
+    else if (url.split('.')[1]=== 'css') {
   staticHandler(__dirname+'/..'+url, res, 'text/css');
-
-}
-else {
+  }
+    else {
 res.writeHead(404);
 res.end('File not found');
+  }
 }
-
-}
-
 module.exports=router;
