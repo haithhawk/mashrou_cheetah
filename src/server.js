5d403953451e67a3fc2bac9b7f1f38b05b36d7cd
Ghassan <ghassan.maslamani@gmail.com>
2017-11-30 11:17:48 +0200

    Showing full adress in the teminal when server start listening
    Relates #38

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
 var http = require("http");
 var router = require("./router");
 var port = process.env.PORT || 3003;
+var hostname = process.env.HOST || "localhost";
 var server = http.createServer(router);
 
 server.listen(port, () => {
-  console.log("server is listening on port " + port);
+  console.log(`Server is listening at http://${hostname}:${port}`);
 });
