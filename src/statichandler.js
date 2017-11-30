29517516e3f5a724000e69e4b3f3f160efc3cb1d
Ghassan <ghassan.maslamani@gmail.com>
2017-11-30 11:57:30 +0200

    Sending error message to the front-end when there is error reading file
    Relates #41

--- src/statichandler.js
+++ src/statichandler.js
@@ -1,27 +1,18 @@
-var fs = require('fs');
+var fs = require("fs");
 
-const staticHandler=(filePath,res, type)=>{
-fs.readFile(filePath , (err,file)=>{
-if (err){
+const staticHandler = (filePath, res, type) => {
+  fs.readFile(filePath, (err, file) => {
+    if (err) {
+      console.log(err);
+      res.writeHead(500);
+      res.end("Sorry there was an error our end");
+    } else {
+      res.writeHead(200, {
+        "Content-Type": type
+      });
+      res.end(file);
+    }
+  });
+};
 
-  console.log("Sorry there was an error our end" + err);
-res.writeHead(500);
-res.end();
-}else{
-
-res.writeHead(200,{
-'Content-Type':type
-
-});
-res.end(file);
-
-}
-
-
-});
-
-
-
-}
-
-module.exports=staticHandler;
+module.exports = staticHandler;
